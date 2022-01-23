1745cd6b5793f574e82084ab4016a681c7a4ca83
ceres1105 <ceres1105@gmail.com>
2022-01-23 14:53:58 +0900

    new: admin page 생성

---
+++ src/pages/adminEmployee/AdminEmployee.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminEmployee = () => {
+  return <div>EMMMM</div>;
+};
+
+export default AdminEmployee;
---
+++ src/pages/adminRoom/AdminRoom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminRoom = () => {
+  return <>RRROOOM</>;
+};
+
+export default AdminRoom;
