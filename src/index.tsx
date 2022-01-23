9cbeb5b8feaaca204fa32f78d3f249266cc56300
ceres1105 <ceres1105@gmail.com>
2022-01-23 15:47:39 +0900

    add: global css, theme css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./services/routes";
+import GlobalStyle from "./styles/global";
+import Theme from "./styles/theme";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>,
+  <ThemeProvider theme={Theme}>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.querySelector("#root"),
 );
